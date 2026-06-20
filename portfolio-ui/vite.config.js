9e3820a87e21f4ece6f1ed2b31f32502c637c740
thomasx <thomas.longobardi.work@gmail.com>
2026-06-20 13:04:46 -0500

    -added proxy to  reroute to local host for local dev

--- portfolio-ui/vite.config.js
+++ portfolio-ui/vite.config.js
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    port: 3000, // The port your frontend runs on locally
+    proxy: {
+      // Intercept any request starting with /api
+      '/api': {
+        target: 'http://localhost:8000', // Forward to local FastAPI backend
+        changeOrigin: true,
+        secure: false,
+        // Optional: Remove /api prefix before hitting FastAPI if your
+        // FastAPI routes don't explicitly start with /api
+        //rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
